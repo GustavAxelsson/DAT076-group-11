37bcf2cf232f7a8c9f440c93ea0ddaed2de957fa
GustavAxelsson <gustav.93@live.se>
2021-02-02 19:55:35 +0100

    Added about page module

--- webshop/src/main/frontend/src/app/app.module.ts
+++ webshop/src/main/frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,

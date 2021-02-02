39fe5599f6ffbf6bb804203653d651386041af84
Linus Johansson <>
2021-02-02 20:00:42 +0100

    Added routing to about page

--- webshop/src/main/frontend/src/app/app-routing.module.ts
+++ webshop/src/main/frontend/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ProductsComponent} from "./pages/products/products.component";
 import {HomeComponent} from "./pages/home/home.component";
+import {AboutComponent} from "./pages/about/about.component";
 
 const routes: Routes = [
   {
@@ -15,6 +16,10 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
   }
 ];
 

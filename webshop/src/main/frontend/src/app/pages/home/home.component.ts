8bd12fd6d6698fdef58d3120d482cae3f516f10b
Linus Johansson <>
2021-02-04 16:12:52 +0100

    Added jquery and fixed next and prev buttons on home.

--- webshop/src/main/frontend/src/app/pages/home/home.component.ts
+++ webshop/src/main/frontend/src/app/pages/home/home.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('carousel') carousel: any
+
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
+
+  nextSlide() {
+    $('.carousel').carousel('next');
+  }
+
+  previousSlide() {
+    $('.carousel').carousel('prev')
+  }
+
+  ngAfterViewInit() {
+  }
+
 
 }

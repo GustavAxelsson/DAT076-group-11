6dda79989c5ff7de2108bcb0e8da647ca5c21f98
Linus Johansson <>
2021-02-08 21:40:55 +0100

    added serializable to entity

--- database/src/main/java/model/entity/Product.java
+++ database/src/main/java/model/entity/Product.java
@@ -7,12 +7,13 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.io.Serializable;
 
 @Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-public class Product {
+public class Product implements Serializable {
     @Id
     private Long id;
     private String name;

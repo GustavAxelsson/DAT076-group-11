9c865d27a6b66ed0b088d8c64d84114ae3c87352
Linus Johansson <>
2021-02-10 18:46:50 +0100

    Added a list of products

--- database/src/test/java/model/dao/ProductOrderDAOTest.java
+++ database/src/test/java/model/dao/ProductOrderDAOTest.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import model.entity.Customer;
+import model.entity.Product;
 import model.entity.ProductOrder;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -13,6 +14,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import javax.ejb.EJB;
+import java.util.ArrayList;
 
 @RunWith(Arquillian.class)
 public class ProductOrderDAOTest {
@@ -21,6 +23,7 @@ public class ProductOrderDAOTest {
         return ShrinkWrap.create(WebArchive.class)
                 .addClasses(ProductOrderDAO.class, ProductOrder.class)
                 .addClasses(CustomerDAO.class, Customer.class)
+                .addClasses(ProductDAO.class, Product.class)
                 .addAsResource("META-INF/persistence.xml")
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
     }
@@ -30,15 +33,30 @@ public class ProductOrderDAOTest {
     @EJB
     private CustomerDAO customerDAO;
 
+    @EJB
+    private ProductDAO productDAO;
+
     @Before
     public void init() {
         Customer customer = new Customer();
         customer.setName("Gurra G");
         customerDAO.create(customer);
 
+        Product nikeShoe = new Product();
+        nikeShoe.setName("Air");
+        nikeShoe.setCategory("Shoe");
+        nikeShoe.setDescription("New nike air");
+        nikeShoe.setPrice(800);
+        productDAO.create(nikeShoe);
+
+        ArrayList<Product> products = new ArrayList<Product>();
+        products.add(nikeShoe);
+
         ProductOrder productOrder = new ProductOrder();
         productOrder.setCustomer(customer);
+        productOrder.setProductList(products);
         productOrderDAO.create(productOrder);
+
     }
 
     @Test

3a360bfa40062f835b81435ba5fe9e2819339b9e
Linus Johansson <>
2021-03-10 21:03:44 +0100

    Updated Jaxrs

--- database/src/main/java/restApi/resources/JAXRSConfiguration.java
+++ database/src/main/java/restApi/resources/JAXRSConfiguration.java
@@ -1,16 +1,24 @@
 package restApi.resources;
 
+import org.eclipse.microprofile.auth.LoginConfig;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
-import org.glassfish.jersey.server.ResourceConfig;
 
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
+@LoginConfig(authMethod = "MP-JWT")
 @ApplicationPath("api")
-public class JAXRSConfiguration extends ResourceConfig {
-   public JAXRSConfiguration() {
-       packages("restApi.resources");
-       packages("restApi.resources").register(MultiPartFeature.class);
-   }
-
-    /* Intentionally left blank */
+public class JAXRSConfiguration extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+        classes.add(MultiPartFeature.class);
+        classes.add(ProductResource.class);
+        classes.add(CategoryResource.class);
+        classes.add(CorsFilter.class);
+        classes.add(AuthService.class);
+        return classes;
+    }
 }

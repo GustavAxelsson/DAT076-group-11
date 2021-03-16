3c22a5e8365529f11f99862e16e316e762da5be5
GustavAxelsson <gustav.93@live.se>
2021-03-16 22:24:19 +0100

    removed duplicate test and imp for hiding the sale text when there is no sale

--- database/src/test/java/model/dao/ProductImageDAOTest.java
+++ database/src/test/java/model/dao/ProductImageDAOTest.java
@@ -29,19 +29,6 @@ public class ProductImageDAOTest extends AbstractDAOTest{
         Assert.assertEquals(1, piList.size());
     }
 
-    /*@Test
-    public void insertProductAndThenAddProductImageTest() {
-        final Product p1 =  new Product("Nike", "https://nike.com", 1200, "Very nice shirt");
-        productDAO.create(p1);
-        byte[] array = new byte[0];
-        ProductImage productImage = new ProductImage("NEW file", array);
-        long id = productDAO.findAll().get(0).getId();
-        Product product = productDAO.getProductById(id);
-        productDAO.updateProductImage(product, productImage);
-        long piId = productImageDAO.findAll().get(0).getId();
-        Assert.assertEquals(productImageDAO.getProductImageById(piId), product.getProductImage());
-    }*/
-
     @Test
     public void getProductImageFromProductId() {
         final Product p1 =  new Product("Nike", "https://nike.com", 1200, "Very nice shirt");
